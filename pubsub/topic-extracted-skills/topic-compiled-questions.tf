4ce54a18d8c69c63270bd3abc494e64ddffc70e8
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-08-24 11:21:29 -0400

    give unique names to resources

--- pubsub/topic-extracted-skills/topic-compiled-questions.tf
+++ pubsub/topic-extracted-skills/topic-compiled-questions.tf
@@ -1,15 +1,15 @@
-resource "google_cloud_run_service_iam_binding" "binding" {
+resource "google_cloud_run_service_iam_binding" "service_binding_toastmaster" {
   location = "us-central1"
   service  = "toastmaster"
   role     = "roles/run.invoker"
   members  = ["allUsers"]
 }
 
-resource "google_pubsub_topic" "default" {
+resource "google_pubsub_topic" "topic_compiled_questions" {
   name = "compiled_questions_topic"
 }
 
-resource "google_pubsub_subscription" "subscription" {
+resource "google_pubsub_subscription" "subscription_toastmaster" {
   name  = "toastmaster_subscription"
-  topic = google_pubsub_topic.default.name
+  topic = google_pubsub_topic.topic_compiled_questions.name
 }
--- pubsub/topic-extracted-skills/topic-extracted-skills.tf
+++ pubsub/topic-extracted-skills/topic-extracted-skills.tf
@@ -1,15 +1,15 @@
-resource "google_cloud_run_service_iam_binding" "binding" {
+resource "google_cloud_run_service_iam_binding" "service_binding_sage" {
   location = "us-central1"
   service  = "sage"
   role     = "roles/run.invoker"
   members  = ["allUsers"]
 }
 
-resource "google_pubsub_topic" "default" {
+resource "google_pubsub_topic" "topic_extracted_skills" {
   name = "extracted_skills_topic"
 }
 
-resource "google_pubsub_subscription" "subscription" {
+resource "google_pubsub_subscription" "subscription_sage" {
   name  = "sage_subscription"
-  topic = google_pubsub_topic.default.name
+  topic = google_pubsub_topic.topic_extracted_skills.name
 }
