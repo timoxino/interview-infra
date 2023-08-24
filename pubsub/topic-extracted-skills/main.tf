a858256d20de7e9f628d1132e2a1d0c71884e0cf
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-08-24 10:45:25 -0400

    move topic config in separate file

--- pubsub/topic-extracted-skills/main.tf
+++ pubsub/topic-extracted-skills/main.tf
@@ -1,20 +1,9 @@
 data "google_project" "project" {
 }
 
-resource "google_pubsub_topic" "default" {
-  name = "extracted_skills_topic"
-}
-
 resource "google_service_account" "sa" {
   account_id   = "cloud-run-pubsub-invoker"
   display_name = "Cloud Run Pub/Sub Invoker"
-}
-
-resource "google_cloud_run_service_iam_binding" "binding" {
-  location = "us-central1"
-  service  = "sage"
-  role     = "roles/run.invoker"
-  members  = ["allUsers"]
 }
 
 resource "google_project_service_identity" "pubsub_agent" {
@@ -27,9 +16,4 @@ resource "google_project_iam_binding" "project_token_creator" {
   project = data.google_project.project.project_id
   role    = "roles/iam.serviceAccountTokenCreator"
   members = ["serviceAccount:${google_project_service_identity.pubsub_agent.email}"]
-}
-
-resource "google_pubsub_subscription" "subscription" {
-  name  = "sage_subscription"
-  topic = google_pubsub_topic.default.name
 }
