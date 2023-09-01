314808abd1d632f631e70d02715029a651aabe5b
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-01 15:10:27 -0400

    bind email from secret

---
+++ binding-secret-sa-toastmaster.tf
@@ -0,0 +1,5 @@
+resource "google_secret_manager_secret_iam_binding" "secret_binding_toastmaster_email_from" {
+  secret_id = "projects/908519591128/secrets/toastmaster-email-from"
+  role      = "roles/secretmanager.secretAccessor"
+  members   = ["serviceAccount:908519591128-compute@developer.gserviceaccount.com"]
+}
