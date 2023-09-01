019fe23fbfba3291e5b72ea9dc396325a3a3f775
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-01 15:24:23 -0400

    bind spring  mail user

--- binding-secret-sa-toastmaster.tf
+++ binding-secret-sa-toastmaster.tf
@@ -15,3 +15,9 @@ resource "google_secret_manager_secret_iam_binding" "secret_binding_toastmaster_
   role      = "roles/secretmanager.secretAccessor"
   members   = ["serviceAccount:908519591128-compute@developer.gserviceaccount.com"]
 }
+
+resource "google_secret_manager_secret_iam_binding" "secret_binding_toastmaster_spring-mail-username" {
+  secret_id = "projects/908519591128/secrets/toastmaster-spring-mail-username"
+  role      = "roles/secretmanager.secretAccessor"
+  members   = ["serviceAccount:908519591128-compute@developer.gserviceaccount.com"]
+}
