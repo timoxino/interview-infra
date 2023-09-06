bbe058c869537e5805ab1af828cacbe4b2b0f142
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-06 10:05:57 -0400

    refer to the bucket name

--- binding-bucket-sa-cv.tf
+++ binding-bucket-sa-cv.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket_iam_binding" "bucket_binding_cv" {
-  bucket = google_storage_bucket.gcs_bucket_cv.interview_cv
+  bucket = google_storage_bucket.gcs_bucket_cv.name
   role   = "roles/storage.objectAdmin"
 
   members = [
