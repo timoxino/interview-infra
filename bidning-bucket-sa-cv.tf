0f406031a018f1b3791984dc81559a89a46a8b23
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-06 09:46:05 -0400

    create bucket for CVs

---
+++ bidning-bucket-sa-cv.tf
@@ -0,0 +1,8 @@
+resource "google_storage_bucket_iam_binding" "bucket_binding_cv" {
+  bucket = google_storage_bucket.gcs_bucket_cv.interview-cv
+  role   = "roles/storage.objectAdmin"
+
+  members = [
+    "serviceAccount:908519591128-compute@developer.gserviceaccount.com"
+  ]
+}
