576639ddbb0768f79b806ec49f010bd7b655de77
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-20 16:37:15 -0400

    specify template for service

--- cloud-run-services.tf
+++ cloud-run-services.tf
@@ -1,14 +1,35 @@
 resource "google_cloud_run_service" "service_sage" {
   name     = "sage"
   location = "us-central1"
+  template {
+    spec {
+      containers {
+        image = "us-central1-docker.pkg.dev/timoxino/interview/sage:latest"
+      }
+    }
+  }
 }
 
 resource "google_cloud_run_service" "service_toastmaster" {
   name     = "toastmaster"
   location = "us-central1"
+  template {
+    spec {
+      containers {
+        image = "us-central1-docker.pkg.dev/timoxino/interview/toastmaster:latest"
+      }
+    }
+  }
 }
 
 resource "google_cloud_run_service" "service_tamer" {
   name     = "tamer"
   location = "us-central1"
+  template {
+    spec {
+      containers {
+        image = "us-central1-docker.pkg.dev/timoxino/interview/tamer:latest"
+      }
+    }
+  }
 }
