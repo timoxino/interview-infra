d314cf707eac8c58c48c036ae01a70d673dff2dc
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-20 07:44:13 -0400

    add cv topic with binding

---
+++ topic-pending-cv.tf
@@ -0,0 +1,15 @@
+resource "google_cloud_run_service_iam_binding" "service_binding_tamer" {
+  location = "us-central1"
+  service  = "tamer"
+  role     = "roles/run.invoker"
+  members  = ["serviceAccount:${google_service_account.sa_cloud_run.email}"]
+}
+
+resource "google_pubsub_topic" "topic_pending_cv" {
+  name = "pending_cv_topic"
+}
+
+resource "google_pubsub_subscription" "subscription_tamer" {
+  name  = "tamer_subscription"
+  topic = google_pubsub_topic.topic_pending_cv.name
+}
